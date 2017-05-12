c00d9ccc306f90d98cdbca4b8c2a396c2fffeacb
ypandoo <ypandoo@126.com>
2017-05-12 11:34:07 +0800

    Initial Commit

---
+++ fastlane/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  fastlane
+//
+//  Created by 杨雷 on 2017/5/12.
+//  Copyright © 2017年 gi. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
